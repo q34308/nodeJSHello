7d30d2229b30d5421ea46d69177404b5463acda7
q34308 <q34308@yahoo.de>
2018-11-21 14:03:26 +0100

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
-var http = require('http');
+var https = require('https');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
-}).listen(8080); 
+    res.end('Hello World! This is a Kudu deployed NodeJS Application.');
+}).listen(443); 
