a582b908245df7862dd8881c5f20c5cbcc1017c4
q34308 <q34308@yahoo.de>
2018-11-21 14:19:47 +0100

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,6 +1,9 @@
-var https = require('https');
+//NodeJS Example using port supplied by Azure Cloud Environment
+var http = require('http');
+var port = process.env.port || 1337;
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World! This is a Kudu deployed NodeJS Application.');
-}).listen(443); 
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello again, World\n');
+}).listen(port);
+
